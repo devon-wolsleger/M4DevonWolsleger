bc86a79c3a95be08ce75bbe15de656154b3f2ab2
Devon Wolsleger <dwolsleg@byu.edu>
2023-01-30 21:16:08 -0700

    Add project files.

---
+++ M4DevonWolsleger/wwwroot/js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿//Jquery selectors to output the proper final grade
+$("#BtnSubmit").click(function () {
+    var assg = $("#assg").val();
+    var group = $('#group').val();
+    var midterm = $('#midterm').val();
+    var final = $('#final').val();
+    var intex = $('#intex').val();
+
+    var total = (parseInt(assg)*.5)+(parseInt(group)*.1)+(parseInt(midterm)*.1)+(parseInt(final)*.1)+(parseInt(intex)*.1);
+
+    var display = "Your final grade in this class will be " + total + "%.";
+
+    $('#output1').html(display);
+
+})
