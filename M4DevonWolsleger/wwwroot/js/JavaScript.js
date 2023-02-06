f60ab0aa8e7bdf79bae1b4299fc0a1c5a4f26769
Devon Wolsleger <dwolsleg@byu.edu>
2023-02-06 15:38:36 -0700

    Jquery

--- M4DevonWolsleger/wwwroot/js/JavaScript.js
+++ M4DevonWolsleger/wwwroot/js/JavaScript.js
@@ -8,24 +8,24 @@ $("#BtnSubmit").click(function () {
 
     var total = (parseInt(assg)*.5)+(parseInt(group)*.1)+(parseInt(midterm)*.1)+(parseInt(final)*.1)+(parseInt(intex)*.1);
 
-    $(document).ready(function () {
-        var percentage = parseFloat($(total).val());
-        var letterGrade;
 
-        if (percentage >= 90) {
-            letterGrade = "A";
-        } else if (percentage >= 80) {
-            letterGrade = "B";
-        } else if (percentage >= 70) {
-            letterGrade = "C";
-        } else if (percentage >= 60) {
-            letterGrade = "D";
-        } else {
-            letterGrade = "F";
-        }
+    var percentage = parseFloat($(total).val());
+    var letterGrade;
 
-        $("#letterGrade").val(letterGrade);
-    });
+    if (percentage >= 90) {
+        letterGrade = "A";
+    } else if (percentage >= 80) {
+        letterGrade = "B";
+    } else if (percentage >= 70) {
+        letterGrade = "C";
+    } else if (percentage >= 60) {
+        letterGrade = "D";
+    } else {
+        letterGrade = "F";
+    }
+
+    $("#letterGrade").val(letterGrade);
+
 
     var display = "Your final grade in this class will be " + total + "%. Which is an " + letterGrade;
 
