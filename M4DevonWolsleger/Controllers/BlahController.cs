fe243c85bc8389dc37c0b2d3310c52e932a9cc71
Devon Wolsleger <dwolsleg@byu.edu>
2023-02-06 15:22:06 -0700

    Mission 5 chnages

---
+++ M4DevonWolsleger/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using M4DevonWolsleger.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M4DevonWolsleger.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalc ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc (GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ M4DevonWolsleger/Models/GradeCalcModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M4DevonWolsleger.Models
+{
+    public class GradeCalcModel
+    {
+        //Validation that makese sure the number is within the range neccessary for grades
+        [Range(0, 100)]
+        public float Assignment { get; set; }
+        [Range(0, 100)]
+        public float GroupProj { get; set; }
+        [Range(0, 100)]
+        public float Quizzes { get; set; }
+        [Range(0, 100)]
+        public float MidtermEx { get; set; }
+        [Range(0, 100)]
+        public float FinalEx { get; set; }
+        [Range(0, 100)]
+        public float Intex { get; set; }
+
+    }
+}
